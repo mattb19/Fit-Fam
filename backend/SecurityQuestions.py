150fe679dc6f6498197c26cb60970b3e8f3c3f76
jbjones1125 <jacobjones5833@gmail.com>
2022-10-04 18:03:40 -0400

    added/edited code skeleton to User.py, SiteRole.py, and SecurityQuestions.py

--- backend/SecurityQuestions.py
+++ backend/SecurityQuestions.py
@@ -1,2 +1,5 @@
-class SecurityQuestions():
-    pass
+class SecurityQuestions:
+  def __init__(self, userId, questionString, answerString):
+    self.__userId = userId
+    self.__questionString = questionString #2 questions delimited via commas
+    self.__answerString = answerString #2 answers delimited via commas
--- backend/SiteRole.py
+++ backend/SiteRole.py
@@ -1,2 +1,4 @@
-class SiteRole():
-    pass
+class SiteRole:
+  def __init__(self, userId):
+    self.__userId = []
+    self.__permissions = 0000000 #string will be indexed into for bool permissions
