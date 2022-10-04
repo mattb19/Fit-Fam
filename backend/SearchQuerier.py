b4578b4198a1ccb46bf2c18a0d7ab39101848a03
Michael-T-Miller <mtm3319@uncw.edu>
2022-10-04 18:05:38 -0400

    added SearchQuerier.py file and edited the existing Profile.py file

---
+++ backend/SearchQuerier.py
@@ -0,0 +1,7 @@
+def searchQuerier(input):
+    '''
+    This function will handle the check of a user input to see if it can be used a query in sql and do the conversion
+    :param tags: Str of input from user
+    Return: string that will be used by the feed to query the SQL server
+    '''
+    pass
