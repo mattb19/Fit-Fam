18a8fe0eed499ca5c3c99c259368d3979d0a23bc
jbjones1125 <jacobjones5833@gmail.com>
2022-10-08 21:32:59 -0400

    new security questions page

---
+++ frontend/src/components/securityQuestionsView.vue
@@ -0,0 +1,88 @@
+<style scoped>
+.addmargin {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.home {
+  background-color: #383c44;
+}
+</style>
+
+<template>
+  <div class="searchView">
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/materia/bootstrap.min.css"
+      integrity="sha384-B4morbeopVCSpzeC1c4nyV0d0cqvlSAfyXVfrPJa25im5p+yEN/YmhlgQP/OyMZD"
+      crossorigin="anonymous"
+    />
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="http://localhost:8080/">FitFam</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor02"
+          aria-controls="navbarColor02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor02">
+          <ul class="navbar-nav me-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Global</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/groups">Groups</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/profile">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/search">Search</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h3 class="large centeralign">Security Questions</h3>
+    <p></p>
+    <p></p>
+    <p>{{ backend }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      backend: "",
+    };
+  },
+  methods: {
+    getStats() {
+      const path = "http://127.0.0.1:5000/home";
+      axios
+        .get(path)
+        .then((res) => {
+          this.backend = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getStats();
+  },
+};
+</script>
