c74570a4ed4668332ab0b0a6e9663572d9743a12
mattb19 <80365359+mattb19@users.noreply.github.com>
2022-11-13 16:34:00 -0500

    removed redundant post tab from navbar

--- frontend/src/components/createPost.vue
+++ frontend/src/components/createPost.vue
@@ -75,9 +75,6 @@
             <li class="nav-item">
               <a class="nav-link" href="/signup">Sign Up</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="/post">Post</a>
-            </li>
           </ul>
         </div>
       </div>
